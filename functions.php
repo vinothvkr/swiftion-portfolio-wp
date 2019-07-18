0293d82195a3efc45f54347fd5ba9103fc42c0aa
vinothvkr <vinothvkr@hotmail.com>
2019-07-18 15:49:49 +0530

    Add sidebar widget

--- functions.php
+++ functions.php
@@ -185,8 +185,19 @@ function swiftionportfolio_widgets_init() {
 
 	register_sidebar(
 		array(
-			'name'          => __( 'Footer', 'swiftionportfolio' ),
+			'name'          => __( 'Sidebar', 'swiftionportfolio' ),
 			'id'            => 'sidebar-1',
+			'description'   => __( 'Add widgets here to appear in sidebar.', 'swiftionportfolio' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+	register_sidebar(
+		array(
+			'name'          => __( 'Footer', 'swiftionportfolio' ),
+			'id'            => 'sidebar-2',
 			'description'   => __( 'Add widgets here to appear in your footer.', 'swiftionportfolio' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
@@ -194,7 +205,6 @@ function swiftionportfolio_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
-
 }
 add_action( 'widgets_init', 'swiftionportfolio_widgets_init' );
 
