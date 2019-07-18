416d5efd279e3021d58c6a78901ed5fe7671e98b
vinothvkr <vinothvkr@hotmail.com>
2019-07-18 15:51:18 +0530

    Add sidebar widget

--- template-parts/sidebar/sidebar-widgets.php
+++ template-parts/sidebar/sidebar-widgets.php
@@ -7,14 +7,14 @@
  * @since 1.0.0
  */
 
-if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
 	<aside class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Footer', 'swiftionportfolio' ); ?>">
 		<?php
-		if ( is_active_sidebar( 'sidebar-2' ) ) {
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
 			?>
 					<div class="widget-column footer-widget-1">
-					<?php dynamic_sidebar( 'sidebar-2' ); ?>
+					<?php dynamic_sidebar( 'sidebar-1' ); ?>
 					</div>
 				<?php
 		}
