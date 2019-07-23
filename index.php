6cca4b501b54638cbb6fd5816859b214d81bbb31
vinothvkr <vinothvkr@hotmail.com>
2019-07-23 22:45:09 +0530

    Add container row divs in index.php

--- index.php
+++ index.php
@@ -16,32 +16,34 @@
 
 get_header();
 ?>
+<div class="container">
+	<div class="row">
+		<section id="primary" class="col-md-8 content-area">
+			<main id="main" class="site-main">
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+			<?php
+			if ( have_posts() ) {
 
-		<?php
-		if ( have_posts() ) {
+				// Load posts loop.
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'template-parts/content/content', 'excerpt' );
+				}
 
-			// Load posts loop.
-			while ( have_posts() ) {
-				the_post();
-				get_template_part( 'template-parts/content/content' );
+				// Previous/next page navigation.
+				swiftionportfolio_the_posts_navigation();
+
+			} else {
+
+				// If no content, include the "No posts found" template.
+				get_template_part( 'template-parts/content/content', 'none' );
+
 			}
+			?>
 
-			// Previous/next page navigation.
-			swiftionportfolio_the_posts_navigation();
-
-		} else {
-
-			// If no content, include the "No posts found" template.
-			get_template_part( 'template-parts/content/content', 'none' );
-
-		}
-		?>
-
-		</main><!-- .site-main -->
-	</section><!-- .content-area -->
-
-<?php
-get_footer();
+			</main><!-- .site-main -->
+		</section><!-- .content-area -->
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+<?php get_footer(); ?>
