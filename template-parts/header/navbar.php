70482a4d5be44136818d91cc8156a4da5360c2e1
vinothvkr <vinothvkr@hotmail.com>
2019-07-23 22:40:50 +0530

    add seperate navbar for home and blog page

---
+++ template-parts/header/navbar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-xl fixed-top swiftion-navbar">
+    <div class="container">
+        <a class="navbar-brand" href="/"><i class="fas fa-fire fa-rotate-270 fa-lg"></i> Swiftion</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="/#header">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="/#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="/#service">Service</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link page-scroll active" href="/blog/">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="/#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
