b8ae3feb78e804d78bc4f7332ef50e60431b9ce7
vinothvkr <vinothvkr@hotmail.com>
2019-07-24 16:50:47 +0530

    Single page design

--- single.php
+++ single.php
@@ -11,50 +11,53 @@
 
 get_header();
 ?>
+<div class="container">
+	<div class="row">
+		<section id="primary" class="col-md-8 content-area">
+			<main id="main" class="site-main">
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+				<?php
 
-			<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+					get_template_part( 'template-parts/content/content', 'single' );
 
-				get_template_part( 'template-parts/content/content', 'single' );
+					if ( is_singular( 'attachment' ) ) {
+						// Parent post navigation.
+						the_post_navigation(
+							array(
+								/* translators: %s: parent post link */
+								'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'swiftionportfolio' ), '%title' ),
+							)
+						);
+					} elseif ( is_singular( 'post' ) ) {
+						// Previous/next post navigation.
+						the_post_navigation(
+							array(
+								'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'swiftionportfolio' ) . '</span> ' .
+									'<span class="screen-reader-text">' . __( 'Next post:', 'swiftionportfolio' ) . '</span> <br/>' .
+									'<span class="post-title">%title</span>',
+								'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'swiftionportfolio' ) . '</span> ' .
+									'<span class="screen-reader-text">' . __( 'Previous post:', 'swiftionportfolio' ) . '</span> <br/>' .
+									'<span class="post-title">%title</span>',
+							)
+						);
+					}
 
-				if ( is_singular( 'attachment' ) ) {
-					// Parent post navigation.
-					the_post_navigation(
-						array(
-							/* translators: %s: parent post link */
-							'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'swiftionportfolio' ), '%title' ),
-						)
-					);
-				} elseif ( is_singular( 'post' ) ) {
-					// Previous/next post navigation.
-					the_post_navigation(
-						array(
-							'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'swiftionportfolio' ) . '</span> ' .
-								'<span class="screen-reader-text">' . __( 'Next post:', 'swiftionportfolio' ) . '</span> <br/>' .
-								'<span class="post-title">%title</span>',
-							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'swiftionportfolio' ) . '</span> ' .
-								'<span class="screen-reader-text">' . __( 'Previous post:', 'swiftionportfolio' ) . '</span> <br/>' .
-								'<span class="post-title">%title</span>',
-						)
-					);
-				}
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+				endwhile; // End of the loop.
+				?>
 
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
+			</main><!-- #main -->
+		</section><!-- #primary -->
+		<?php get_sidebar(); ?>
+	</div>
+</div>
 <?php
 get_footer();
--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -12,11 +12,19 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( ! swiftionportfolio_can_show_post_thumbnail() ) : ?>
 	<header class="entry-header">
-		<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
-	</header>
-	<?php endif; ?>
+		<?php swiftionportfolio_post_thumbnail() ?>
+		<?php
+		if ( is_sticky() && is_home() && ! is_paged() ) {
+			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'swiftionportfolio' ) );
+		}
+		the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-meta">
+		<?php swiftionportfolio_entry_meta(); ?>
+	</div><!-- .entry-meta -->
 
 	<div class="entry-content">
 		<?php
