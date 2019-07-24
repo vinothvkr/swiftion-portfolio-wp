cf908bf454c4c5299d4e2776ed9fd72809f80005
vinothvkr <vinothvkr@hotmail.com>
2019-07-24 17:28:01 +0530

    Archive page styling

--- archive.php
+++ archive.php
@@ -11,44 +11,47 @@
 
 get_header();
 ?>
+<div class="container">
+	<div class="row">
+		<section id="primary" class="col-md-8 content-area">
+			<main id="main" class="site-main">
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+			<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+				<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+					?>
+				</header><!-- .page-header -->
 
-			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				?>
-			</header><!-- .page-header -->
+				// Start the Loop.
+				while ( have_posts() ) :
+					the_post();
 
-			<?php
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();
+					/*
+					* Include the Post-Format-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					*/
+					get_template_part( 'template-parts/content/content', 'excerpt' );
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content/content', 'excerpt' );
+					// End the loop.
+				endwhile;
 
-				// End the loop.
-			endwhile;
+				// Previous/next page navigation.
+				swiftionportfolio_the_posts_navigation();
 
-			// Previous/next page navigation.
-			swiftionportfolio_the_posts_navigation();
+				// If no content, include the "No posts found" template.
+			else :
+				get_template_part( 'template-parts/content/content', 'none' );
 
-			// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content/content', 'none' );
-
-		endif;
-		?>
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
+			endif;
+			?>
+			</main><!-- #main -->
+		</section><!-- #primary -->
+		<?php get_sidebar(); ?>
+	</div>
+</div>
 <?php
 get_footer();
