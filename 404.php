9a3434780dd745ad3bf57a8ccbfa2fbd6b323840
vinothvkr <vinothvkr@hotmail.com>
2019-07-24 20:34:42 +0530

    404 page styling

--- 404.php
+++ 404.php
@@ -11,23 +11,35 @@
 
 get_header();
 ?>
+<div class="container">
+	<div class="row">
+		<section id="primary" class="col-md-12 content-area">
+			<main id="main" class="site-main">
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+				<div class="error-404 not-found">
+					<header class="page-header">
+						<h1 class="page-title">404</h1>
+					</header><!-- .page-header -->
 
-			<div class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'swiftionportfolio' ); ?></h1>
-				</header><!-- .page-header -->
+					<div class="page-content">
+						<p><?php _e( 'We are sorry, But the page you requested was not found', 'swiftionportfolio' ); ?></p>
+						<a href="/" class="btn btn-primary">Go Home</a>
+						<a href="/#contact" class="btn btn-outline-primary">Contact Us</a>
+					</div><!-- .page-content -->
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'swiftionportfolio' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</div><!-- .error-404 -->
+					<div class="page-footer">
+						<form class="form-inline justify-content-center search-form" role="search" method="get" action="/" >
+							<div class="form-group mr-3">
+								<input type="search" class="form-control search-field" placeholder="Search" value name="s">
+							</div>
+							<button type="submit" class="btn btn-primary">Search</button>
+						</form>
+					</div>
+				</div><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
+			</main><!-- #main -->
+		</section><!-- #primary -->
+	</div>
+</div>
 <?php
 get_footer();
