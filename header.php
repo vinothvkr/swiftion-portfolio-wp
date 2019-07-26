60d2e8c7a78141cce3b8574ef6b939d49b958b23
vinothvkr <vinothvkr@hotmail.com>
2019-07-26 19:40:37 +0530

    Add google fonts

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
